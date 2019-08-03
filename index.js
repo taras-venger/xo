adbd00fdc2ed7d44deed8f7f7e98de53f5557c88
Taras Venger <taras-venger@users.noreply.github.com>
2019-08-03 21:39:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,64 +2,66 @@ let state = {
     players: [
         {
             name: 'Player 1',
-            moves: [], // contains array of cells' coordinates: [[1, 1], [1, 2]...]
+            moves: [],
             mark: 'X',
+            winner: false
         },
         {
             name: 'Player 2',
-            moves: [], // contains array of cells' coordinates: [[1, 1], [1, 2]...]
+            moves: [],
             mark: '0',
+            winner: false
         },
     ],
     activePlayer: 0,
     gameOver: false
 };
 
-const initialState = JSON.parse(JSON.stringify(state));
+const cellsIDs = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+const cellsPosition = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]];
+const cellsDOM = document.querySelectorAll('.cell');
 
-function cellAvailability(cell) {
-    const selected = [...state.players[0].moves, ...state.players[1].moves];
-    const selectedStr = selected.map(el => el.toString());
-    return selectedStr.indexOf(cell.toString()) === -1;
+const getUsedIDs = () => [...state.players[0].moves, ...state.players[1].moves];
+const getState = state => JSON.parse(JSON.stringify(state));
+const initialState = getState(state);
+
+
+function cellAvailability(id) {
+    const usedIDs = getUsedIDs();
+    return !usedIDs.some(usedID => id === usedID);
 };
 
-// reseives an array of player's moves
-const matchByRowOrColumn = arr => {
-    const benchmark = arr[0];
-    let x = 0;
-    let y = 0;
-    for (let el of arr) {
-        el[0] === benchmark[0] ? x++ : '';
-        el[1] === benchmark[1] ? y++ : '';
+const matchByRowOrColumn = moves => {
+    const lastMove = moves[moves.length - 1];
+    let row = 0;
+    let column = 0;
+    for (let i of moves) {
+        cellsPosition[i][0] === cellsPosition[lastMove][0] && row++;
+        cellsPosition[i][1] === cellsPosition[lastMove][1] && column++;
     };
-    return x === 3 || y === 3;
+    return row === 3 || column === 3;
 };
 
-// reseives an array of player's moves
-const matchByDiagonal = arr => {
+const matchByDiagonal = moves => {
     let d1 = 0;
-    let d2 = 0
-    for (let el of arr) {
-        el[0] === el[1] ? d1++ : '';
-        el[0] + el[1] === 4 ? d2++ : '';
+    let d2 = 0;
+    for (let i of moves) {
+        cellsPosition[i][0] === cellsPosition[i][1] && d1++;
+        cellsPosition[i][0] + cellsPosition[i][1] === 4 && d2++;
     };
     return d1 === 3 || d2 === 3;
 };
 
-const checkWin = arr => matchByRowOrColumn(arr) || matchByDiagonal(arr);
+const checkWin = moves => matchByRowOrColumn(moves) || matchByDiagonal(moves);
+const checkDraw = () => getUsedIDs().length === 9;
 
-const checkDraw = () => {
-    const selectedCells = [...state.players[0].moves, ...state.players[1].moves];
-    return selectedCells.length === 9 && !state.gameOver;
-};
-
-function gameOver(arr, name) {
-    if (checkWin(arr)) {
+function gameOver(moves, name) {
+    if (checkWin(moves)) {
         state.gameOver = true;
         setTimeout(() => alert(`${name} won!`), 0);
     } else if (checkDraw()) {
         state.gameOver = true;
-        setTimeout(() => alert(`Game over.`), 0);
+        setTimeout(() => alert(`Game over`), 0);
     };
 };
 
@@ -67,21 +69,18 @@ function switchPlayer() {
     state.activePlayer === 0 ? state.activePlayer = 1 : state.activePlayer = 0;
 };
 
-function makeMove(coordinates, id) {
+function makeMove(id) {
     if (!state.gameOver) {
-        const activePlayer = state.activePlayer;
-        const name = state.players[activePlayer].name;
-        const moves = state.players[activePlayer].moves;
-        const mark = state.players[activePlayer].mark;
+        const { name, moves, mark } = state.players[state.activePlayer];
         // Player's move
-        if (cellAvailability(coordinates)) {
-            moves.push(coordinates);
+        if (cellAvailability(id)) {
+            moves.push(id);
             document.getElementById(id).innerText = mark;
             gameOver(moves, name);
             // Machine's move
             if (!state.gameOver) {
                 switchPlayer();
-                state.activePlayer === 1 ? machineMove() : '';
+                state.activePlayer === 1 && machineMove();
             }
         } else {
             alert('Selected cell is not available');
@@ -90,37 +89,23 @@ function makeMove(coordinates, id) {
 };
 
 function machineMove() {
-    // Identify available cells
-    const allCells = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]];
-    const selectedCells = [...state.players[0].moves, ...state.players[1].moves];
-    const allCellsStr = allCells.map(cell => cell.toString());
-    const selectedStr = selectedCells.map(cell => cell.toString());
-    const availableCells = allCellsStr.filter(cell => !selectedStr.includes(cell));
-    // Select rendom cell
-    const random = Math.floor(Math.random() * availableCells.length);
-    const randomCell = availableCells[random].split(',');
-    const x = parseInt(randomCell[0]);
-    const y = parseInt(randomCell[1]);
-    const coordinates = [x, y];
-    const id = randomCell.join('-');
-    makeMove(coordinates, id);
+    const usedIDs = getUsedIDs();
+    const vacantIDs = cellsIDs.filter(id => !usedIDs.includes(id));
+    const randomNumber = Math.floor(Math.random() * vacantIDs.length);
+    const randomID = vacantIDs[randomNumber];
+    makeMove(randomID);
 };
 
 function resetGame() {
-    state = JSON.parse(JSON.stringify(initialState));
-    const cells = document.querySelectorAll('.cell');
-    for (let cell of cells) {
+    state = getState(initialState);
+    for (let cell of cellsDOM) {
         cell.innerText = '';
     };
 };
 
 const table = document.querySelector('.table');
 table.addEventListener('click', event => {
-    const x = parseInt(event.target.dataset.x);
-    const y = parseInt(event.target.dataset.y);
-    const coordinates = [x, y];
-    const id = event.target.id;
-    makeMove(coordinates, id);
+    makeMove(event.target.id);
 });
 
 const startButton = document.querySelector('#start');
